a07ae31e65605c6fda0f979906629ec366cec432
ygdrasil-io <alexandre.mommers@gmail.com>
2026-08-25 02:30:41 +0200

    build(buildSrc): restore KMP publication conventions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    implementation("com.android.tools.build:gradle:${libs.versions.agp.get()}")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:${libs.versions.dokka.get()}")
+    implementation("com.vanniktech:gradle-maven-publish-plugin:${libs.versions.maven.publish.get()}")
     implementation("dev.opensavvy.dokka.mkdocs:dokka-mkdocs:0.6.3")
 }
