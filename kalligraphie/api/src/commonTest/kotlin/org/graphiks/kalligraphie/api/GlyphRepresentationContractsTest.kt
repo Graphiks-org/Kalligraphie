f00cb6af8d72dcb5ea2cdd3bd58e546a3cea26f9
ygdrasil-io <alexandre.mommers@gmail.com>
2026-09-18 23:12:41 +0200

    test(font): drop tautological bitmap assertions

--- kalligraphie/api/src/commonTest/kotlin/org/graphiks/kalligraphie/api/GlyphRepresentationContractsTest.kt
+++ kalligraphie/api/src/commonTest/kotlin/org/graphiks/kalligraphie/api/GlyphRepresentationContractsTest.kt
@@ -147,28 +147,6 @@ class GlyphRepresentationContractsTest {
     }
 
     @Test
-    fun colorBitmapRepresentationOwnsItsStraightRgbaPixels() {
-        val decodedPixels = byteArrayOf(10, 20, 30, 40)
-        val bitmap = BitmapGlyphIR(
-            glyphId = GlyphId(4),
-            strike = BitmapStrike(16, 16, 32),
-            width = 1,
-            height = 1,
-            originX = 0,
-            originY = 0,
-            metrics = BitmapGlyphMetrics(advanceX = 16, advanceY = 0),
-            pixelFormat = BitmapPixelFormat.RGBA_8888,
-            colorSpace = GlyphColorSpace.SRGB,
-            decodedPixels = decodedPixels,
-        )
-        decodedPixels[0] = 99
-
-        assertEquals(4, bitmap.decodedByteCount)
-        assertEquals(BitmapPixelFormat.RGBA_8888, bitmap.pixelFormat)
-        assertContentEquals(byteArrayOf(10, 20, 30, 40), bitmap.copyDecodedPixels())
-    }
-
-    @Test
     fun colorBitmapRepresentationRejectsAPixelBufferThatDoesNotMatchItsDimensions() {
         assertFailsWith<IllegalArgumentException> {
             BitmapGlyphIR(
